ac3a94e3003f3932a244448778908dc528a07294
ando-malala <rakotoniainaandomalala@gmail.com>
2026-02-06 08:57:59 +0300

    Database conception

---
+++ bdd/conception.sql
@@ -0,0 +1,50 @@
+CREATE DATABASE bdd_voiture;
+
+\c bdd_voiture;
+
+-- COMMENT
+
+-- LE BUT C'EST D'AVOIR L'ORDRE DE DEPART DES VEHICULES DE L'AEROPORT VERS LES HOTELS
+
+create Table vehicule(
+    id serial primary key,
+    cpacite INT NOT NULL,
+    type enum('D', 'E') NOT NULL,
+);
+
+create Table parametre(
+    id serial primary key,
+    vitesseMoyenne INT NOT NULL,
+    tempsAttente INT NOT NULL
+);
+
+create table hotel (
+    id serial primary key,
+    nom VARCHAR(100) NOT NULL,
+    adresse VARCHAR(255) NOT NULL
+);
+
+CREATE hotelDistance (
+    id serial primary key,
+    idHotel INT NOT NULL,
+    distance FLOAT NOT NULL, -- en KM
+    FOREIGN KEY (idHotel) REFERENCES hotel(id)
+);
+
+create table reservation (
+    id serial primary key,
+    idHotel INT NOT NULL,
+    infoClient VARCHAR(255) NOT NULL,
+    nbPassager INT NOT NULL,
+    dateHeure TIME NOT NULL, -- arrive des clients à l'aéroport
+    FOREIGN KEY (idHotel) REFERENCES hotel(id)
+);
+
+create table ordreDepart (
+    id serial primary key,
+    idVehicule INT NOT NULL,
+    idReservation INT NOT NULL,
+    heureDepart TIME NOT NULL, -- heure de départ du véhicule de l'aéroport
+    FOREIGN KEY (idVehicule) REFERENCES vehicule(id),
+    FOREIGN KEY (idReservation) REFERENCES reservation(id)
+);
